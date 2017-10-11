f65badfd135c97b64b86c5b4557cdb501a8ad6f0
ahoang10 <ahoang10@jhu.edu>
2017-10-11 06:50:06 -0400

    create view for posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,3 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
 ]
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 3586ac526b3f92fa3b771ac16a826a66c6a428d1
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-<<<<<<< HEAD
-=======
 
->>>>>>> 3586ac526b3f92fa3b771ac16a826a66c6a428d1
 from django.conf.urls import include, url
 from django.contrib import admin
 
